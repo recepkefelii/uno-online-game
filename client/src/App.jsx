db194b805f9e01a36c3d4ced94f86897508c1db5
recepkefelii <recepkefeliii@gmail.com>
2023-01-11 20:07:59 +0300

    add react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import {Route, Routes} from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Routes>
+      <Route path='/' element={""}/>
+      <Route path='/login' element={""}/>
+      <Route path='/games' element={""}/>
+      <Route path='/create-game' element={""}/>
+      <Route path='game/:gameid' element={""} />
+    </Routes>
   )
 }
 

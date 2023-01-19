2b10048ac67540748482018b82d8c20b7be52b89
recepkefelii <recepkefeliii@gmail.com>
2023-01-19 18:16:42 +0300

    add get-rooms hook

--- client/src/hooks/Socket/Room/GetRooms.js
+++ client/src/hooks/Socket/Room/GetRooms.js
@@ -1,13 +1,19 @@
-import { useEffect } from "react"
-import { io } from "socket.io-client"
+import useSocket from "../Socket";
+import { useEffect,useState } from "react";
 
 const useGetRooms = () => {
+    const socket = useSocket()
+    const [rooms, setRooms] = useState([]);
     useEffect(() => {
-        const socket = io(`${import.meta.env.VITE_SERVER}${nickname}`)
-        socket.on("allRooms", (data) => {
-            console.log(data);
-        })
-    },[])  
+        if(socket){
+            socket.on('allRooms', (allRooms) => {
+                setRooms(allRooms);
+            });
+            socket.emit('getRooms');
+        }
+    }, [socket]);
+    return rooms;
 }
 
-export {socket,useGetRooms};
+
+export default useGetRooms;

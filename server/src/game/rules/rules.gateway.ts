5691edb2b3c3f080fada4a3b31c8441ff2390fe0
recepkefelii <recepkefeliii@gmail.com>
2023-02-07 17:52:25 +0300

    refactor: getGameId and username

--- server/src/game/rules/rules.gateway.ts
+++ server/src/game/rules/rules.gateway.ts
@@ -2,7 +2,6 @@ import { Body, Injectable } from "@nestjs/common";
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, WsException } from "@nestjs/websockets";
 import { Socket } from "dgram";
 import { Server } from "socket.io"
-import { GetCards } from "./dto/cards.dto";
 import { Rules } from "./rules.service";
 import { CheckMatchService } from "./service/match/check-match.service";
 
@@ -19,11 +18,16 @@ export class RulesGateway {
   ) { }
   @WebSocketServer() server: Server
 
+  private async getGameIdAndUsername(socket: any): Promise<[number, string]> {
+    const username = socket.handshake.query.username;
+    const gameId = await this.checkService.checkUserMatch(username);
+    return [gameId, username];
+  }
+
   @SubscribeMessage('getCards')
   async getCards(@ConnectedSocket() socket: any) {
     try {
-      const username = socket.handshake.query.username;
-      const gameId = await this.checkService.checkUserMatch(username);
+      const [gameId, username] = await this.getGameIdAndUsername(socket);
       const cards = await this.rulesService.getPlayerCards(gameId, username)
       this.server.emit('play', cards)
       return cards
@@ -34,10 +38,14 @@ export class RulesGateway {
 
   @SubscribeMessage('getMainCard')
   async getMainCards(@ConnectedSocket() socket: any) {
-    const username = socket.handshake.query.username;
-    const gameId = await this.checkService.checkUserMatch(username)
-    const mainCard = await this.rulesService.getMainCards(gameId)
-    this.server.emit('play', mainCard)
-    return mainCard
+    try {
+      const [gameId] = await this.getGameIdAndUsername(socket);
+      const mainCard = await this.rulesService.getMainCards(gameId)
+      this.server.emit('play', mainCard)
+      return mainCard
+    } catch (error) {
+      throw new WsException(error.message);
+    }
   }
+
 }

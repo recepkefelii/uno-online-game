a716759f244ec7a58cbd87bfbbf9d55d63990448
recepkefelii <recepkefeliii@gmail.com>
2023-02-12 22:29:20 +0300

    add: check-match-service logger

--- server/src/game/rules/service/match/check-match.service.ts
+++ server/src/game/rules/service/match/check-match.service.ts
@@ -15,9 +15,10 @@ export class CheckMatchService extends GameRules {
         });
 
         if (!user || !user.game || !user.game.id) {
+            this.logger.log(`User ${username} is not in any game`)
             throw new WsException("User or user's game not found",);
         }
-
+        this.logger.log(`Found in which room ${username} is`)
         return user.game.id;
     }
 }

4ee9e59771aade1b2e02a9bcf3751bcb65dfb68a
recepkefelii <recepkefeliii@gmail.com>
2023-02-12 22:41:55 +0300

    add: user service logger

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Player } from '../entities/player.entity';
@@ -7,19 +7,29 @@ import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserService {
+    logger: Logger
     constructor(@InjectRepository(Player)
     private playerRepository: Repository<Player>,
-    ) { }
+    ) {
+        this.logger = new Logger(UserService.name)
+    }
 
     async newUser(body: userDto) {
-        const player = new Player()
-        const saltRounds = 10;
-        const hashName = await bcrypt.hash(body.name, saltRounds);
-        player.hash = hashName
-        player.name = body.name
-        player.currentTurn = false
+        try {
+            const player = new Player()
+            const saltRounds = 10;
+            const hashName = await bcrypt.hash(body.name, saltRounds);
+            player.hash = hashName
+            player.name = body.name
+            player.currentTurn = false
 
-        const user = await this.playerRepository.save(player)
-        return { nickname: user.name }
+            const user = await this.playerRepository.save(player)
+            this.logger.log(`User named ${player.name} has been successfully created`)
+            return { nickname: user.name }
+        } catch (error) {
+            this.logger.error(`Error while creating user: ${error}`)
+            throw new HttpException('This username is already used', HttpStatus.BAD_REQUEST)
+        }
     }
+
 }

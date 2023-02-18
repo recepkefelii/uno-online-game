2fa03fc766992e3b696aaa638fabafc81404f9cf
recepkefelii <recepkefeliii@gmail.com>
2023-02-18 21:34:23 +0300

    add: login cache and redis

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,17 @@
-import { Global, Module } from '@nestjs/common';
+import { CacheModule, CacheStore, Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './config/orm.config';
 import { GameModule } from './game/game.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Global()
 @Module({
   imports: [TypeOrmModule.forRoot(ormConfig), GameModule, AuthModule, ConfigModule.forRoot({
-     isGlobal: true,
-     envFilePath: '.env',
-  })],
-  controllers: [],
-  providers: [ConfigService],
+    isGlobal: true,
+    envFilePath: '.env',
+  }),CacheModule.register({ store: redisStore as unknown as CacheStore, host: 'localhost', port: 6379, }),
+  ]
 })
 export class AppModule { }

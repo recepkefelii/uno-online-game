5b160f7dfb24cb2ec1de9177722d64aa1ffe56a2
recepkefelii <recepkefeliii@gmail.com>
2023-02-23 21:50:51 +0300

    add: socket module

--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -1,35 +1,51 @@
-import {OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer} from '@nestjs/websockets';
-import {Server, Socket} from "socket.io";
-import {UnauthorizedException} from "@nestjs/common";
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from "socket.io";
+import { UnauthorizedException, UseFilters } from "@nestjs/common";
 import { AuthGuard } from 'src/shared/guard/auth.guard';
-
-@WebSocketGateway({cors: {origin: ['http://localhost:3000', 'http://localhost:4200','http://localhost:5173']}})
-export class SocketGateWay extends AuthGuard implements  OnGatewayConnection, OnGatewayDisconnect{
+import { WsCatchAllFilter } from 'src/exception/ws-filter';
+import { SocketService } from './socket.service';
+import { IGetUserType } from 'src/game/interface/user.interface';
+import { GameSocket } from './game.socket';
+import { WsUnauthorizedException } from 'src/exception/ws-exceptions';
+@UseFilters(new WsCatchAllFilter())
+@WebSocketGateway({ cors: { origin: ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:5173'] } })
+export class SocketGateWay implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private readonly socketService: SocketService
+    
+    ) { }
 
   @WebSocketServer()
   server: Server;
 
-  async handleConnection(socket: Socket) {
+  async handleConnection(socket: GameSocket) {
     try {
-    const user = this.validateToken(socket.handshake.headers.authorization);
+      const user = this.socketService.validateToken(socket.handshake.headers.authorization) as unknown as IGetUserType
+      const gameId = await this.socketService.getUserGameId(user.name)
   
-      if(!user){
-        console.log('disconnect users');
+      if (!gameId) socket.disconnect()
+  
+      if (!user) {
         return this.disconnect(socket);
-      } 
+      }
+  
+      socket.gameId = gameId
+      socket.join(gameId.toString())
+      console.log(socket.rooms);
+      
+  
     } catch (error) {
-      console.log(error)
       return this.disconnect(socket);
     }
   }
+  
 
-  async handleDisconnect(socket: Socket) {
+  async handleDisconnect(socket: GameSocket) {
     socket.disconnect();
   }
 
 
-  private disconnect(socket: Socket) {
-    socket.emit('Error', new UnauthorizedException());
-    socket.disconnect();
+  private disconnect(socket: GameSocket) {
+    throw new WsUnauthorizedException('helloo')
   }
 }

12bae909182e09e2bae8b228c4f6aedec7d41571
recepkefelii <recepkefeliii@gmail.com>
2023-03-03 01:45:51 +0300

    update: vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react(), eslint()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.js']
+  },
 })

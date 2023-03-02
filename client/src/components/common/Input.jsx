d2b2a6f7ce5312221d91a977c3fb47dd5de2e765
recepkefelii <recepkefeliii@gmail.com>
2023-03-03 01:49:10 +0300

    chore: input component

--- client/src/components/common/Input.jsx
+++ client/src/components/common/Input.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 
-const Input = ({placeholder}) => {
-    return (
-        <>
-            <input
-                placeholder={`${placeholder}`} type="text" className='placeholder-white pl-4 text-white cursor-text 
-            placeholder- bg-blue4 h-9 w-full
-            border-none outline-none rounded-md  shadow-md shadow-blue1' />
-        </>
-    )
+const Input = ({ placeholder }) => {
+	return (<>
+		<input
+			placeholder={`${placeholder}`} type="text" className='placeholder-white pl-4 text-white cursor-text 
+      placeholder- bg-blue4 h-9 w-full
+      border-none outline-none rounded-md  shadow-md shadow-blue1' />
+	</>
+	)
 }
 
 export default Input

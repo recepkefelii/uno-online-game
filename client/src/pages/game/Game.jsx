d8be66d5321c0685f81d4bcd1d81c4e7f93289c4
recepkefelii <recepkefeliii@gmail.com>
2023-03-06 15:19:23 +0300

    release: 0.6 version

---
+++ client/src/pages/game/Game.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BlueCard from '../../../public/cards/front-blue.png'
+
+const Game = () => {
+    return (
+        <div>
+            <img src={BlueCard} alt=""  width={150}/>
+        </div>
+    )
+}
+
+export default Game

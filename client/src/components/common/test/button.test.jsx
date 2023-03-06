b94157d9d02c671847c31ef48be3e499ac96c869
recepkefelii <recepkefeliii@gmail.com>
2023-03-06 15:56:29 +0300

    done

--- client/src/components/common/test/button.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Button from '../Button';
-
-test('renders button component with correct text', () => {
-  const text = 'Click Me';
-  render(<Button text={text} />);
-  const buttonElement = screen.getByText(text);
-  expect(buttonElement).toBeInTheDocument();
-});
--- client/src/components/common/test/input.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Input from '../Input';
-
-test('renders input component with correct placeholder', () => {
-  const placeholder = 'Username';
-  render(<Input placeholder={placeholder} />);
-  const inputElement = screen.getByPlaceholderText(placeholder);
-  expect(inputElement).toBeInTheDocument();
-});
